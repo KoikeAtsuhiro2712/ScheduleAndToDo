bd6666ca7c7df87e93879c0a690312695e61160d
KoikeAtsuhiro2712 <atsuhiro.koike@neotex.co.jp>
2025-10-24 14:23:02 +0900

    Wip 2025/10/24
    戻るボタンの共通化
    他の画面コンポーネントはこれから

---
+++ ScheduleAndToDo/Views/CommonUI/BackButton.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ScheduleAndToDo.Views.CommonUI;
+
+/// <summary>
+/// BackButton.xaml の相互作用ロジック
+/// </summary>
+public partial class BackButton : UserControl
+{
+    public BackButton()
+    {
+        InitializeComponent();
+    }
+    public static readonly DependencyProperty CommandProperty =
+        DependencyProperty.Register(nameof(Command),typeof(ICommand),typeof(BackButton));
+    public ICommand Command
+    {
+        get => (ICommand)GetValue(CommandProperty);
+        set => SetValue(CommandProperty,value);
+    }
+}
