3a204cb7446ff77e8b71a59544e1ceff882ca19f
KoikeAtsuhiro2712 <atsuhiro.koike@neotex.co.jp>
2025-10-27 17:55:06 +0900

    MOD
    ボタンを共通化させる

---
+++ ScheduleAndToDo/Views/CommonUI/AddButton.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ScheduleAndToDo.Views.CommonUI
+{
+    /// <summary>
+    /// AddButton.xaml の相互作用ロジック
+    /// </summary>
+    public partial class AddButton : UserControl
+    {
+        public AddButton()
+        {
+            InitializeComponent();
+        }
+    public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(AddButton));
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+    }
+}
---
+++ ScheduleAndToDo/Views/CommonUI/NormarlButton.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ScheduleAndToDo.Views.CommonUI
+{
+    /// <summary>
+    /// NormarlButton.xaml の相互作用ロジック
+    /// </summary>
+    public partial class NormarlButton : UserControl
+    {
+        public NormarlButton()
+        {
+            InitializeComponent();
+        }
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(nameof(Command),typeof(ICommand),typeof(NormarlButton));
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+    }
+}
